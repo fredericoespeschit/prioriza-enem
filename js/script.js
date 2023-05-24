cdf4e96050870d3f2d0af79ed223c951aadb5589
fredericoespeschit <fespeschitt@gmail.com>
2023-05-24 15:47:19 -0300

    Implementação de CRUD com localStorage em JavaScript, create e read

--- js/script.js
+++ js/script.js
@@ -3,5 +3,29 @@
 const openModal = () => document.getElementById('modal-add-new-question').classList.add('active');
 const closeModal = () =>  document.getElementById('modal-add-new-question').classList.remove('active');
 
+const tempQuestion = {
+  question: "Quanto é 2 + 2?",
+  level: "1", //1 é fácil, 5 é dificil
+  matter: "Matematica",
+  content: "Adição"
+}
+
+const getLocaStorage = () => JSON.parse(localStorage.getItem('dataBase')) ?? [];
+
+const setLocaStorage = (dataBase) => localStorage.setItem('dataBase', JSON.stringify(dataBase));
+
+//CRUD
+const readItem = () => {
+  getLocaStorage();
+}
+
+const createItem = (questions) => {
+  const dataBase = getLocaStorage()
+  dataBase.push(questions);
+  setLocaStorage(dataBase);
+}
+
+
+// Events
 document.getElementById('register-question').addEventListener('click', openModal);
 document.getElementById('modal-close').addEventListener('click', closeModal);
