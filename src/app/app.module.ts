425b47c75ac318c1e5f48b916e75f3cd0490dbef
fredericoespeschit <fespeschitt@gmail.com>
2023-06-04 13:59:54 -0300

    Componentização do HTML

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { MainComponent } from './components/shared/main/main.component';
+import { ModalComponent } from './components/shared/modal/modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
